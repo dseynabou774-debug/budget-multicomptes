3e826ae5256b05dd0a9fcb8f2cc8e69b50e27213
dseynabou774-debug <dseynabou774@gmail.com>
2026-09-13 16:26:51 +0000

    Add files via upload

---
+++ sw-4.js
@@ -0,0 +1,25 @@
+// Incrémente ce numéro à chaque nouvelle mise à jour du fichier index.html
+// pour forcer le rechargement du cache sur les téléphones qui ont déjà installé l'appli.
+const CACHE_VERSION = 'sza-budget-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_VERSION).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
